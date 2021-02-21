9e7ce3ee842ddb685604ffb786cb3eb2512c97e2
Ofilia Cudd <ofiliacudd@gmail.com>
2021-02-20 23:37:26 -0500

    spunch update

--- actions/spunchbob.js
+++ actions/spunchbob.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-const images = 12;
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     check(message) {
@@ -7,8 +8,12 @@ module.exports = {
         return words[0] === 'b!spunch';
     },
     execute(message) {
-        var choice = Math.round(Math.random() * images);
-        const file = new Discord.MessageAttachment(__dirname + '/../pics/spunchbob/' + choice + '.jpg');
+        const filesList = fs.readdirSync(
+            path.resolve(`${__dirname}/../pics/spunchbob/`)
+        );
+        const random = Math.round(Math.random() * (filesList.length - 1));
+
+        const file = new Discord.MessageAttachment(path.resolve(`${__dirname}/../pics/spunchbob/${filesList[random]}`));
         message.channel.send({ files: [file] });
     }
 }
