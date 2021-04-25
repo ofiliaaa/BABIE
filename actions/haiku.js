1a1acb78c5ee0484643b8051b0d4af76481a7ab4
Ofilia Cudd <ofiliacudd@gmail.com>
2021-04-24 22:58:34 -0400

    hehe formst

--- actions/haiku.js
+++ actions/haiku.js
@@ -1,32 +1,28 @@
 const haiku = [
-    'this is a haiku, this line has more syllables, please be nice to me', 
-    'look at butter dog, hehe she is so stinky, butter dog is queen',
-    'TS jones is back, everybody hide your feets, n!tickle attack', 
-    'im the coolest bot, my swag is unparalleled, waffle is cool too', 
-    'bring back mount garfield, love that big fact orange cat, he sure hates mondays',
-    'aaa italians, mama mia pizza pie, cooka da meatball', 
-    'battleblock theater, everybody needs to play, *sings* buckle your pants',
-    'oh no jones is ts, try your best to run away, you will not escape (written by Jones)',
-    'sydney may protest, but i think its obvious: animal fetish (written by jones)',
-    'oh was that a yawn, someone should head straight to sleep, but you likely wont (written by jones)',
-    'little slimy slug, hes so squishy what a king, dont you dare salt him',
-    'i love my slug son, he is just so beautiful, what a little king',
+    "this is a haiku\nthis line has more syllables\nplease be nice to me",
+    "look at butter dog\nhehe she is so stinky\nbutter dog is queen",
+    "TS jones is back\neverybody hide your feets\nn!tickle attack",
+    "im the coolest bot\nmy swag is unparalleled\nwaffle is cool too",
+    "bring back mount garfield\nlove that big fat orange cat\nhe sure hates mondays",
+    "aaa italians\nmama mia pizza pie\ncooka da meatball",
+    "battleblock theater\neverybody needs to play\n*sings* buckle your pants",
+    "oh no jones is ts\ntry your best to run away\nyou will not escape (written by Jones)",
+    "sydney may protest\nbut i think its obvious: animal fetish (written by jones)",
+    "oh was that a yawn\nsomeone should head straight to sleep\nbut you likely wont (written by jones)",
+    "little slimy slug\nhes so squishy what a king\ndont you dare salt him",
+    "i love my slug son\nhe is just so beautiful\nwhat a little king",
 ];
 
-var count = Math.round(Math.random() * (haiku.length - 1));
+
 
 module.exports = {
     check(message) {
-        var words = message.content.toLowerCase().split(' ');
-        return words[0] === 'b!haiku';
+        var words = message.content.toLowerCase().split(" ");
+        return words[0] === "b!haiku";
     },
     execute(message) {
+        var count = Math.round(Math.random() * (haiku.length - 1));
         message.channel.send(haiku[count]);
-        // increment the counter by one
-        count++;
-        // if the counter is past the facts count, reset it to zero
-        if (count >= haiku.length){
-            count = Math.round(Math.random() * (haiku.length - 1));
-        }
+     
     }
 }
