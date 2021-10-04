82e57a528894a3e7717fe0583ea54d0587025f0e
Ofilia Cudd <ofiliacudd@gmail.com>
2021-10-03 21:04:48 -0400

    spookie

--- actions/afraid.js
+++ actions/afraid.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const triggers = ['im scared','im so scared','i am scared', 'i am so scared', 'youre scaring me', 'are you scared', 'what are you scared', 'are you scared yet', 'i am afraid', 'im afraid', 'im so afraid', 'i am so afraid', 'ooo im so scared'];
+const triggers = ['im scared','im so scared','i am scared', 'i am so scared', 'youre scaring me', 'are you scared', 'what are you scared', 'are you scared yet', 'i am afraid', 'im afraid', 'im so afraid', 'i am so afraid', 'ooo im so scared', 'thats terrifying', 'thats so terrifying','im terrified', 'i am terrified', 'i am so terrified'];
 
 module.exports = {
     check(message) {
