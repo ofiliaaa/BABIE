1a9f768c5dcc3c14e7025fa998189c5c87a93ce2
Ofilia Cudd <ofiliacudd@gmail.com>
2021-10-21 01:05:14 -0400

    waiting for crochet lol

--- actions/foff.js
+++ actions/foff.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 
 module.exports = {
     check(message) {
-        return message.content.toLowerCase() === ('hmm');
+        return message.content.toLowerCase().trim() === ('fuck off');
     },
     execute(message) {
-        const file = new Discord.MessageAttachment(__dirname + '/../pics/hmm.jpg');
+        const file = new Discord.MessageAttachment(__dirname + '/../pics/foff.jpg');
         message.channel.send({ files: [file] });
     }
 }
--- actions/fuck.js
+++ actions/fuck.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     check(message) {
-        var words = message.content.toLowerCase().trim() === ('fuck');
+        return message.content.toLowerCase().trim() === ('fuck');
     },
     execute(message) {
         const file = new Discord.MessageAttachment(__dirname + '/../pics/fuck.jpg');
