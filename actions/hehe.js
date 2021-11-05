c2bc4ca083e610835020d9214ce7da524d0f5e1b
Ofilia Cudd <ofiliacudd@gmail.com>
2021-11-05 14:14:40 -0400

    hehe

---
+++ actions/hehe.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const triggers = ['hehe','heehee','hehehehe','hehehe','hehehehehehe'];
+
+module.exports = {
+    check(message) {
+        return triggers.includes(message.content.toLowerCase().replace(/[\u0300-\u036F]/g, "")
+        .replace(/[\u2018\u2019]/g, "")
+        .replace(/[\u201C\u201D]/g, "")
+        .replace(/[']/g,"")
+        .replace(/\W\s/g, ''))
+    },
+    execute(message) {
+        const file = new Discord.MessageAttachment(__dirname + '/../pics/insane.jpg');
+        message.channel.send({ files: [file] });
+    }
+}
