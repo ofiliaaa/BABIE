d026545445ccb169a4a704e38affec2845ec3866
Ofilia Cudd <ofiliacudd@gmail.com>
2021-11-05 21:29:30 -0400

    lying

--- actions/lying.js
+++ actions/lying.js
@@ -1,12 +1,19 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     check(message) {
-        var words = message.content.toLowerCase().split(' ');
-        return words[0] === ('b!lying');
+        var words = message.content.toLowerCase().split(' ')
+        return words[0] === 'b!lying';
     },
     execute(message) {
-        const file = new Discord.MessageAttachment(__dirname + '/../pics/lying.jpg');
+        const filesList = fs.readdirSync(
+            path.resolve(`${__dirname}/../pics/lie/`)
+        );
+        const random = Math.round(Math.random() * (filesList.length - 1));
+
+        const file = new Discord.MessageAttachment(path.resolve(`${__dirname}/../pics/lie/${filesList[random]}`));
         message.channel.send({ files: [file] });
     }
 }
