bc6b8595d7c55efe7d7369a4810a2b80c9314a47
Ofilia Cudd <ofiliacudd@gmail.com>
2021-11-05 21:48:14 -0400

    noah fancy things

--- actions/hehe.js
+++ actions/hehe.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const triggers = ['hehe','heehee','hehehehe','hehehe','hehehehehehe'];
 
 module.exports = {
     check(message) {
-        return triggers.includes(message.content.toLowerCase().replace(/[\u0300-\u036F]/g, "")
+        message = message.content.toLowerCase().replace(/[\u0300-\u036F]/g, "")
         .replace(/[\u2018\u2019]/g, "")
         .replace(/[\u201C\u201D]/g, "")
         .replace(/[']/g,"")
-        .replace(/\W\s/g, ''))
+        .replace(/\W\s/g, '');
+
+        message = message.split('').filter((item, index, array) => {
+            if (index === 0) {
+                return true;
+            } else if (item === array[index - 1]) {
+                return false;
+            } else {
+                return true;
+            }
+        }).join('');
+
+        if (message.length < 4) return false;
+        return message.split("he").filter(e => e).length === 0;
     },
     execute(message) {
         const file = new Discord.MessageAttachment(__dirname + '/../pics/hehe.jpg');
