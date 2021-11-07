b8a7fbb13e34b5ff02b97655a4a3fb606d248428
Ofilia Cudd <ofiliacudd@gmail.com>
2021-11-07 17:24:29 -0500

    aaa?

--- actions/cringe.js
+++ actions/cringe.js
@@ -5,7 +5,8 @@ const triggers = ['cringe'];
 
 module.exports = {
     check(message) {
-        return triggers.includes(message.content.toLowerCase().replace(/[\u0300-\u036F]/g, "").replace(/[\u2018\u2019]/g, "").replace(/[\u201C\u201D]/g, "").replace(/[']/g,"").replace(/\W\s/g, ''))
+        return triggers.includes(message.content.toLowerCase().replace(/\W\s/g, '')
+        )
     },
     execute(message) {
         const filesList = fs.readdirSync(
