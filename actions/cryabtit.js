a7e3a7cc6b5ee1f0f50cbcdc227ae3e051f37877
Ofilia Cudd <ofiliacudd@gmail.com>
2021-11-14 20:24:44 -0500

    no more orgo pls

---
+++ actions/cryabtit.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const triggers = ['cry about it', 'cry abt it'];
+
+module.exports = {
+    check(message) {
+        return triggers.includes(message.content.toLowerCase()
+        .replace(/[\u0300-\u036F]/g, "")
+        .replace(/[\u2018\u2019]/g, "")
+        .replace(/[\u201C\u201D]/g, "")
+        .replace(/[']/g,"")
+        .replace(/\W\s/g, ''))
+    },
+    execute(message) {
+        const file = new Discord.MessageAttachment(__dirname + '/../pics/cryabtit.jpg');
+        message.channel.send({ files: [file] });
+    }
+}
